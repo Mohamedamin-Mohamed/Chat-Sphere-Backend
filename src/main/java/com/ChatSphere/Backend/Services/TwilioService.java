da35f25b4a64541586cdf5a214e587633681edd6
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-20 19:48:07 -0500

    feat: add new service class for twilio business logic

--- src/main/java/com/ChatSphere/Backend/Services/TwilioService.java
+++ src/main/java/com/ChatSphere/Backend/Services/TwilioService.java
@@ -1,22 +1,45 @@
 package com.ChatSphere.Backend.Services;
 
 import com.ChatSphere.Backend.Config.TwilioConfig;
-import com.ChatSphere.Backend.Dto.TwilioVerifyDto;
+import com.ChatSphere.Backend.Dto.TwilioSendPinDto;
+import com.ChatSphere.Backend.Dto.TwilioVerifyPinDto;
 import com.twilio.rest.verify.v2.service.Verification;
+import com.twilio.rest.verify.v2.service.VerificationCheck;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class TwilioVerify {
-    private TwilioConfig twilioConfig;
+@Slf4j
+public class TwilioService {
+    private final TwilioConfig twilioConfig;
     @Value("${twilio.VERIFICATION_SID}")
     String serviceId;
 
-    public boolean sendCode(TwilioVerifyDto twilioVerifyDto) {
-        twilioConfig.connect();
-        Verification verification = Verification.creator(serviceId, twilioVerifyDto.getPhoneNumber(), "sms").create();
-        return verification.getValid();
+    public boolean sendCode(TwilioSendPinDto sendPinDto) {
+        try {
+            twilioConfig.connect();
+            Verification verification = Verification.creator(serviceId, sendPinDto.getPhoneNumber(), "sms").create();
+            return verification.getStatus().equals("pending");
+        } catch (Exception exp) {
+            log.info("Something went wrong", exp);
+        }
+        return false;
     }
+
+    public boolean verifyCode(TwilioVerifyPinDto verifyPinDto) {
+        try {
+            twilioConfig.connect();
+            VerificationCheck verificationCheck = VerificationCheck.
+                    creator(serviceId, verifyPinDto.getCode()).
+                    setTo(verifyPinDto.getPhoneNumber()).create();
+            return verificationCheck.getStatus().equals("approved");
+        } catch (Exception exp) {
+            log.error("Something went wrong", exp);
+        }
+        return false;
+    }
+
 }
