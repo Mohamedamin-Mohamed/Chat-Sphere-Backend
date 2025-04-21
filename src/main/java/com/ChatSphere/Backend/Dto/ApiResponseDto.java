cee09cdfe0492aa015ac5543cf527d1ad6d6b5e5
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-20 19:44:32 -0500

    feat: add new dto clas for twilio api response

--- src/main/java/com/ChatSphere/Backend/Dto/ApiResponseDto.java
+++ src/main/java/com/ChatSphere/Backend/Dto/ApiResponseDto.java
@@ -1,4 +1,14 @@
 package com.ChatSphere.Backend.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
 public class ApiResponseDto {
+    private boolean success;
+    private String message;
+    private Object data;
+
 }
