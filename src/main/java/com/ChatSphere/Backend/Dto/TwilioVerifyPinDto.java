327355f43f88eb134d88e4fb7c38c0f63d483b77
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-20 19:45:28 -0500

    feat: add new dto class for incoming request for twilio pin verification

--- src/main/java/com/ChatSphere/Backend/Dto/TwilioVerifyPinDto.java
+++ src/main/java/com/ChatSphere/Backend/Dto/TwilioVerifyPinDto.java
@@ -1,4 +1,10 @@
 package com.ChatSphere.Backend.Dto;
 
+import lombok.Data;
+
+@Data
 public class TwilioVerifyPinDto {
+    private String email;
+    private String phoneNumber;
+    private String code;
 }
