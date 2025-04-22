b7f2a2ae4da936187d75d73cfa8bd3acfed764b9
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-21 21:27:00 -0500

    feat: add new dto class for response when sending pin

--- src/main/java/com/ChatSphere/Backend/Dto/SendCodeResultDto.java
+++ src/main/java/com/ChatSphere/Backend/Dto/SendCodeResultDto.java
@@ -1,4 +1,13 @@
 package com.ChatSphere.Backend.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class SendCodeResultDto {
+    private boolean success;
+    private String message;
 }
