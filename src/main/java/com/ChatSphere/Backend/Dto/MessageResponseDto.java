26cafba0492eabcce0bb45566d9111bf9a86fab4
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-27 21:08:26 -0500

    feat: add annotation to class

--- src/main/java/com/ChatSphere/Backend/Dto/MessageResponseDto.java
+++ src/main/java/com/ChatSphere/Backend/Dto/MessageResponseDto.java
@@ -1,4 +1,14 @@
 package com.ChatSphere.Backend.Dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MessageResponseDto {
+    private String messageId;
+    private String email;
+    private String sender;
+    private String message;
+    private String timestamp;
 }
