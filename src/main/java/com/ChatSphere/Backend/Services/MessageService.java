288be9818ec34e85a5b00d7f70a41a69cff68172
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-27 21:13:07 -0500

    feat: add new methods for saving and loading of messages

--- src/main/java/com/ChatSphere/Backend/Services/MessageService.java
+++ src/main/java/com/ChatSphere/Backend/Services/MessageService.java
@@ -1,4 +1,35 @@
 package com.ChatSphere.Backend.Services;
 
+import com.ChatSphere.Backend.Dto.MessageRequestDto;
+import com.ChatSphere.Backend.Dto.MessageResponseErrorDto;
+import com.ChatSphere.Backend.Mappers.ModelMapper;
+import com.ChatSphere.Backend.Model.Message;
+import com.ChatSphere.Backend.Repositories.MessageRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class MessageService {
+    private final MessageRepository messageRepository;
+    private final ModelMapper modelMapper;
+
+    public Object saveMessage(MessageRequestDto messageDto) {
+        try {
+            Message message = modelMapper.map(messageDto);
+            messageRepository.save(message);
+            return modelMapper.map(message);
+        } catch (Exception exp) {
+            log.error("Something went wrong", exp);
+            return new MessageResponseErrorDto(false, String.format("Couldn't save %s message with sender of email %s", messageDto.getSender(), messageDto.getEmail()));
+        }
+    }
+
+    public List<Message> loadMessages() {
+        return messageRepository.findAll();
+    }
 }
