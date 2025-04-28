d8159ba2183ddb74204abd18860f36aef97f2e02
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-27 21:29:37 -0500

    feat: add new method for loading messages by user email

--- src/main/java/com/ChatSphere/Backend/Repositories/MessageRepository.java
+++ src/main/java/com/ChatSphere/Backend/Repositories/MessageRepository.java
@@ -4,8 +4,10 @@ import com.ChatSphere.Backend.Model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, UUID> {
+    List<Message> findMessageByEmail(String email);
 }
