738360903d22a545452778e9984c6bba9d027f39
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-27 21:12:46 -0500

    feat: add new repository for chatbot messages

--- src/main/java/com/ChatSphere/Backend/Repositories/MessageRepository.java
+++ src/main/java/com/ChatSphere/Backend/Repositories/MessageRepository.java
@@ -1,4 +1,11 @@
 package com.ChatSphere.Backend.Repositories;
 
-public class MessageRepository {
+import com.ChatSphere.Backend.Model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, UUID> {
 }
