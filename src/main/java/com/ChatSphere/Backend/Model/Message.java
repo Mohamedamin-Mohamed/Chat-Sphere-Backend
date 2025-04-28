b8a8a3acabbe69a242e8364a82f2f5dd62926ce4
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-04-27 21:11:26 -0500

    feat: make email field non-null

--- src/main/java/com/ChatSphere/Backend/Model/Message.java
+++ src/main/java/com/ChatSphere/Backend/Model/Message.java
@@ -1,4 +1,28 @@
 package com.ChatSphere.Backend.Model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "Messages")
+@Getter
+@Setter
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID messageID;
+
+    @NonNull
+    private String email;
+
+    @NonNull
+    private String sender;
+    @NonNull
+    private String message;
+    @NonNull
+    private String timestamp;
 }
