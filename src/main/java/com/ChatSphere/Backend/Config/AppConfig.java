a9bfbc2c440313b1349b5b774a458aded45a9dcd
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-05-01 22:00:30 -0500

    feat: add new configuration class for OpenSearch client

--- src/main/java/com/ChatSphere/Backend/Config/AppConfig.java
+++ src/main/java/com/ChatSphere/Backend/Config/AppConfig.java
@@ -1,4 +1,83 @@
 package com.ChatSphere.Backend.Config;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.json.jackson.JacksonJsonpMapper;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.transport.OpenSearchTransport;
+import org.opensearch.client.transport.rest_client.RestClientTransport;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+@Configuration
 public class AppConfig {
+    @Value("${OpenSearch.url}")
+    String url;
+
+    @Value("${OpenSearch.username}")
+    String userName;
+
+    @Value("${OpenSearch.password}")
+    String password;
+
+
+    @Value("${OpenSearch.port}")
+    int port;
+
+    @Value("${OpenSearch.protocol}")
+    String protocol;
+
+    @Bean
+    CredentialsProvider credentialsProvider() {
+        BasicCredentialsProvider basicCredentialsProvider = new BasicCredentialsProvider();
+        basicCredentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(userName, password));
+        return basicCredentialsProvider;
+    }
+
+    @Bean
+    HttpHost httpHost() {
+        return new HttpHost(url, port, protocol);
+    }
+
+    @Bean
+    SSLContext sslContext() throws NoSuchAlgorithmException, KeyManagementException {
+        return SSLContextBuilder.create().build();
+    }
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder(
+                        new HttpHost(httpHost()))
+                .setHttpClientConfigCallback(httpClientBuilder ->
+                {
+                    try {
+                        return httpClientBuilder
+                                .setDefaultCredentialsProvider(credentialsProvider())
+                                .setSSLContext(sslContext());
+                    } catch (NoSuchAlgorithmException | KeyManagementException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    OpenSearchTransport openSearchTransport() {
+        return new RestClientTransport(restClient(), new JacksonJsonpMapper());
+    }
+
+    @Bean
+    public OpenSearchClient openSearchClient() {
+        return new OpenSearchClient(openSearchTransport());
+    }
 }
