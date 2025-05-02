98a9c8cf386f515c45d5a90f7af843269f7998d0
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-05-01 21:56:17 -0500

    feat: add new dto class when creating or representing embedding info

--- src/main/java/com/ChatSphere/Backend/Dto/EmbeddingDto.java
+++ src/main/java/com/ChatSphere/Backend/Dto/EmbeddingDto.java
@@ -1,11 +1,13 @@
 package com.ChatSphere.Backend.Dto;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+@RequiredArgsConstructor
 @Getter
 @Setter
-public class CreateEmbeddingDto {
+public class EmbeddingDto {
     private String question;
     private String answer;
     private String timestamp;
