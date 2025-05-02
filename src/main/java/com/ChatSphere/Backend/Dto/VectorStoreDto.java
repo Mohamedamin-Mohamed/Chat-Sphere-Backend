97d46712180558317195215364882dd37d6c7d93
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-05-01 21:58:42 -0500

    feat: add new dto class for storing of embedding to open search cluster

--- src/main/java/com/ChatSphere/Backend/Dto/VectorStoreDto.java
+++ src/main/java/com/ChatSphere/Backend/Dto/VectorStoreDto.java
@@ -1,4 +1,15 @@
 package com.ChatSphere.Backend.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class VectorStoreDto {
+    private EmbeddingDto embeddingDto;
+    private float[] embeddings;
 }
