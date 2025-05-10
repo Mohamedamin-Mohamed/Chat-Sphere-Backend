c16cacda470920a01423d4223c3b76a5437fc152
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-05-09 19:37:04 -0500

    feat: add boolean fields

--- src/main/java/com/ChatSphere/Backend/Dto/UserSearchDto.java
+++ src/main/java/com/ChatSphere/Backend/Dto/UserSearchDto.java
@@ -1,4 +1,23 @@
 package com.ChatSphere.Backend.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserSearchDto {
+    private String email;
+    private String name;
+    private String bio;
+    private String picture;
+    private String joinedDate;
+    private boolean isOnline;
+    private int followerSize;
+    private int followingSize;
+    private boolean isFollowedByRequester;
+    private boolean isFollowingRequester;
 }
