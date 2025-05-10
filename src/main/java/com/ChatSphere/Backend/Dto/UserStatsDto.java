10f2f6f03919eb8372b0024bcb5edd5d4aa01efe
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-05-09 19:37:39 -0500

    feat: new class thats used to return for user stats request

--- src/main/java/com/ChatSphere/Backend/Dto/UserStatsDto.java
+++ src/main/java/com/ChatSphere/Backend/Dto/UserStatsDto.java
@@ -1,4 +1,12 @@
 package com.ChatSphere.Backend.Dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserStatsDto {
+    private int followers;
+    private int followings;
 }
