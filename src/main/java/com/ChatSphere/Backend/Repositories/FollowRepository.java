097ccdd0f5d3b44acc2480b13b81bccd848046d5
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-05-09 19:41:19 -0500

    feat: new repository class for follow related requests

--- src/main/java/com/ChatSphere/Backend/Repositories/FollowRepository.java
+++ src/main/java/com/ChatSphere/Backend/Repositories/FollowRepository.java
@@ -1,10 +1,15 @@
 package com.ChatSphere.Backend.Repositories;
 
-import com.ChatSphere.Backend.Model.Follower;
+import com.ChatSphere.Backend.Model.Follow;
+import com.ChatSphere.Backend.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface FollowersRepository extends JpaRepository<Follower, Long> {
+import java.util.List;
 
+@Repository
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+    List<Follow> findFollowsByFollower(User user);
+
+    List<Follow> findByFollower(User user);
 }
