c9388e2613d2221b23d6d7661752636a8c0c9385
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-05-09 19:44:24 -0500

    refac: removed unused annotations

--- src/main/java/com/ChatSphere/Backend/Services/TwilioService.java
+++ src/main/java/com/ChatSphere/Backend/Services/TwilioService.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -30,7 +31,6 @@ public class TwilioService {
 
     private final TwilioConfig twilioConfig;
     private final UserService userService;
-
 
     public SendCodeResultDto sendCode(TwilioSendPinDto sendPinDto) {
         try {
