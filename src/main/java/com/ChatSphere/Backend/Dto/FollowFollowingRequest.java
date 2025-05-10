c37e7d35ff5e5df89f10a1eeabea33fe3ce1ac3a
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-05-09 19:36:07 -0500

    feat: new record to map incoming requests for follow and unfollow

--- src/main/java/com/ChatSphere/Backend/Dto/FollowFollowingRequest.java
+++ src/main/java/com/ChatSphere/Backend/Dto/FollowFollowingRequest.java
@@ -2,12 +2,8 @@ package com.ChatSphere.Backend.Dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-public record FollowRequest(
+public record FollowFollowingRequest(
         @NotBlank(message = "Email is required") @Email(message = "Follower email needs to be valid if provided") String followerEmail,
         @NotBlank(message = "Email is required") @Email(message = "Following email needs to be valid if provided") String followingEmail) {
 }
